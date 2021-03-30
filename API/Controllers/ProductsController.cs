389ad6445d4dc23904136ee33b9c20e3145b093a
githubanis <anissarker301@gmail.com>
2021-03-30 14:42:47 +0600

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var result = await _context.Products.ToListAsync();
+
+            return Ok(result);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> SaveProduct([FromBody])
+    }
+}
