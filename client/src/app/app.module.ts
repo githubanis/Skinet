2dbfa31de8a8800a6b8fd8c0c8491d99faca470e
githubanis <anissarker301@gmail.com>
2021-05-03 23:07:48 +0600

    Routing and lazy loading added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,8 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopComponent } from './shop/shop.component';
-import { ShopModule } from './shop/shop.module';
+import {HomeModule} from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +19,7 @@ import { ShopModule } from './shop/shop.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
